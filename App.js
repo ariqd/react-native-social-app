fe8ca69e85ea2614bfae9db61d4823c9edcd93a4
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-02-26 08:13:10 +0700

    Onboarding UI

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import Onboarding from './screens/Onboarding';
+import OnboardingScreen from './screens/Onboarding';
 import Login from './screens/Login';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const AppStack = createStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <AppStack.Navigator>
-        <AppStack.Screen name="Onboarding" component={Onboarding} />
-        <AppStack.Screen name="Login" component={Login} />
-      </AppStack.Navigator>
-    </NavigationContainer>
-  );
+  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then((value) => {
+      if (value === null) {
+        AsyncStorage.setItem('alreadyLaunched', TRUE);
+        setIsFirstLaunch(TRUE);
+      } else {
+        setIsFirstLaunch(FALSE);
+      }
+    });
+  }, []);
+
+  if (isFirstLaunch === null) {
+    return null;
+  } else if (isFirstLaunch === true) {
+    return (
+      <NavigationContainer>
+        <AppStack.Navigator headerMode="none">
+          <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
+          <AppStack.Screen name="Login" component={Login} />
+        </AppStack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return <Login />;
+  }
 };
 
 export default App;
--- screens/Onboarding.js
+++ screens/Onboarding.js
@@ -1,24 +1,71 @@
 import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper';
 
-const Onboarding = (props) => {
+const Skip = ({...props}) => (
+  <TouchableOpacity style={{marginHorizontal: 8}} {...props}>
+    <Text style={{fontSize: 16}}>Skip</Text>
+  </TouchableOpacity>
+);
+
+const Next = ({...props}) => (
+  <TouchableOpacity style={{marginHorizontal: 8}} {...props}>
+    <Text style={{fontSize: 16}}>Next</Text>
+  </TouchableOpacity>
+);
+
+const Done = ({...props}) => (
+  <TouchableOpacity style={{marginHorizontal: 8}} {...props}>
+    <Text style={{fontSize: 16}}>Done</Text>
+  </TouchableOpacity>
+);
+
+const Dots = ({selected}) => {
+  let backgroundColor = selected ? 'rgba(0,0,0,0.8)' : 'rgba(0,0,0,0.3)';
+
   return (
-    <View style={styles.container}>
-      <Text>Onboarding Screen</Text>
-      <Button
-        title="Click here"
-        onPress={() => props.navigation.navigate('Login')}
-      />
-    </View>
+    <View
+      style={{
+        width: 5,
+        height: 5,
+        marginHorizontal: 3,
+        backgroundColor,
+      }}
+    />
   );
 };
 
-export default Onboarding;
+const OnboardingScreen = ({navigation}) => {
+  return (
+    <Onboarding
+      onSkip={() => navigation.replace('Login')}
+      onDone={() => navigation.navigate('Login')}
+      SkipButtonComponent={Skip}
+      NextButtonComponent={Next}
+      DoneButtonComponent={Done}
+      DotComponent={Dots}
+      pages={[
+        {
+          backgroundColor: '#a6e4d0',
+          image: <Image source={require('../assets/onboarding-img1.png')} />,
+          title: 'Connect to the World',
+          subtitle: 'A new way to connect with the world.',
+        },
+        {
+          backgroundColor: '#fdeb93',
+          image: <Image source={require('../assets/onboarding-img2.png')} />,
+          title: 'Share Your Favorites',
+          subtitle: 'Share your thoughts with similar kind of people.',
+        },
+        {
+          backgroundColor: '#e9bcbe',
+          image: <Image source={require('../assets/onboarding-img3.png')} />,
+          title: 'Become the Star',
+          subtitle: 'Let the spotlight capture you.',
+        },
+      ]}
+    />
+  );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default OnboardingScreen;
